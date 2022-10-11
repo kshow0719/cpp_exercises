ee16d408a57ef0f9ea75e08c2fa11c8a17b9b551
Keisuke Kano <86813026+kshow0719@users.noreply.github.com>
2022-10-11 12:02:55 +0900

    ex2 not finished

--- ex02/test_stack.cpp
+++ ex02/test_stack.cpp
@@ -1,7 +1,11 @@
 // test_stack.cpp
 // Insert the relevant headers here 
 
+#include <stdio.h>
+#include "stack.h"
+
+using namespace std;
+
 int main(void) {
-  // COMPLETE
-  // Write tests for testing your Stack implementation. 
+    
 }

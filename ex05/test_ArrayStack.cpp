d8c8f611ba237b633600b32c60fddb7f09b5e355
Keisuke Kano <86813026+kshow0719@users.noreply.github.com>
2022-10-20 09:06:54 +0900

    Create ex5

---
+++ ex05/test_ArrayStack.cpp
@@ -0,0 +1,14 @@
+// test_ArrayStack.cpp
+#include "ArrayStack.h"
+
+int main(void) {
+// Create an instance of ArrayStack named stack1 
+//   using the default constructor
+// Push several items in this stack.
+
+// Create another instance of ArrayStack named stack2
+//   using the other constructor, specify an original size of 5
+// Push several items in this stack.
+
+return 0;
+}

7b94f80176af32c950ddc5ebe605c0e6d2f4d9a4
Keisuke Kano <86813026+kshow0719@users.noreply.github.com>
2022-11-01 22:02:26 +0900

    ex6_Q3

--- ex06/polygon.cpp
+++ ex06/polygon.cpp
@@ -4,3 +4,70 @@
 #include"polygon.h"
 using namespace std;
 
+// コンストラクタ引数なし
+Polygon::Polygon(){
+    vertex_num = 0;
+    vertex = nullptr;
+}
+
+// コンストラクタ引数あり
+Polygon::Polygon(int vertex_num, Point Vertex[]){
+    this->num_of_vertex = vertex_num;
+    this->vertex = new Point[this->vertex_num];
+    for(int i = 0; i < this-> vertex_num; i++){
+        this->vertex[i].x = Vertex[i].x;
+        this->vertex[i].y = Vertex[i].y;
+    }
+}
+
+// コピーコンストラクタ
+Polygon::Polygon(const Polygon& polygon_cp){
+    this->vertex_num = polygon_cp.vertex_num;
+    this->vertex = new Point[polygon_cp.vertex_num];
+    for(int i = 0; i < polygon_cp.vertex_num; i++){
+        this->vertex[i].x = polygon_cp.vertex[i].x;
+        this->vertex[i].y = polygon_cp.vertex[i].y;
+    }
+}
+
+// コピーの代入演算子
+Polygon& Polygon::operator=(const Polygon& polygon_cp){
+    // 自身の代入チェック
+    if(this != &polygon_cp){
+        delete[] vertex;
+        this->vertex_num = polygon_cp.vertex_num;
+        this->vertex = new Point[polygon_cp.vertex_num];
+        for(int i = 0; i < polygon_cp.vertex_num; i++){
+            this->vertex[i].x = polygon_cp.vertex[i].x;
+            this->vertex[i].y = polygon_cp.vertex[i].y;
+        }
+    }
+    return *this;
+}
+
+string Polygon::get_name(){
+    return "polygon";
+}
+
+float Polygon::compute_area(){
+    float result = 0, temp;
+    for(int i = 0; i < this->vertex_num; i++){
+        temp = (((this->vertex[i].x)*(this->vertex[(i+1) % vertex_num].y)) - ((vertex[i].y)*(vertex[(i+1) % vertex_num].x)));
+        result += temp;
+    }
+    result /= 2;
+    return result;
+}
+
+Polygon* Polygon::create(){
+    Polygon *Pol_new = new Polygon();
+    return Pol_new;
+}
+
+Polygon* Polygon::clone(){
+    Polygon *Pol_clo = new Polygon(*this);
+    return Pol_clo;
+}
+
+Polygon::~Polygon(){
+}

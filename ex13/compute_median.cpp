0d0cc8c7d187e94cebdf8893059a4c6add1f0d21
Keisuke Kano <86813026+kshow0719@users.noreply.github.com>
2022-11-21 11:27:55 +0900

    Complete ex13

---
+++ ex13/compute_median.cpp
@@ -0,0 +1,68 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+using namespace std;
+
+void compute_median(vector<double>& array){
+
+    // begin, endの取得
+    auto ar_begin = begin(array);
+    auto ar_end = end(array);
+
+    // 配列サイズの取得
+    auto ar_size = distance(ar_begin, ar_end);
+    // サイズが偶数か奇数で処理
+    if (ar_size % 2 == 0){
+        nth_element(ar_begin, ar_begin + (ar_size/2 - 1), ar_end);
+        nth_element(ar_begin, ar_begin + (ar_size/2), ar_end);
+    }
+    else{
+        nth_element(ar_begin, ar_begin + (ar_size/2), ar_end);
+    }
+
+    // ソート後の配列を表示
+    cout << "After array: ";
+    for (auto value : array) {
+        cout << value << ", ";
+    }
+    cout << endl;
+
+    // 結果の表示
+    double median;
+    if (ar_size % 2 == 0) {
+        auto l_middle = ar_begin + (ar_size/2 - 1);
+        auto r_middle = ar_begin + (ar_size/2);
+        median = ((*l_middle) + (*r_middle)) / 2;
+    }
+    else{
+        median = *(ar_begin + (ar_size/2));
+    }
+
+    cout << "Median: " << median << endl;
+}
+
+int main(void) {
+    vector<double> vec1 = {55.3, 28.2, -2, 18, 36.2, 4.2, 3.2, -42, -1.3};
+    vector<double> vec2 = {3, 5.2, 6.9, 10, -1, 4.2, 8, 100};
+
+    // test for array1
+    cout << "Before array1: " << endl;
+    for (auto value : vec1) {
+        cout << value << ", ";
+    }
+    cout << endl;
+
+    compute_median(vec1);
+
+    // test for array2
+    cout << "Before array2: " << endl;
+    for (auto value : vec2) {
+        cout << value << ", ";
+    }
+    cout << endl;
+
+    compute_median(vec2);
+
+    return 0;
+}
